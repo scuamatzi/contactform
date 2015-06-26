94860ac8224e30931d01314e6a3d1f2d371b6bbb
scuamatzi <escuamatzi@gmail.com>
2015-06-26 11:52:10 -0500

    html fixed. Useless closing div deleted. Footer deleted. Header deleted.

--- index.php
+++ index.php
@@ -8,103 +8,98 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 	<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
 	<style>
-body {
-    background-color: #C6F5F2;
-}
-#content { background-color:#C6F5F2; width:950px; min-height:550px; text-align:left; padding:20px;  }
-h1 {
-    padding:20px 10px 20px 10px;
-}
-h2 {
-    padding-left: 0px !important;
-}
-#header {
-    background-color: #CA410B !important;
-}
-.large { font-size:22px; }
-.orange { color:orange; }
-.italic { font-style:italic }
-.textmiddle {vertical-align:middle;}
-.padout { padding-left:25px; padding-right:25px; }
-.rounded-corners {
-     -moz-border-radius: 40px;
-    -webkit-border-radius: 40px;
-    -khtml-border-radius: 40px;
-    border-radius: 40px;
-}
-.rounded-corners-top {
-     -moz-border-top-radius: 40px;
-    -webkit-border-radius: 40px;
-    -khtml-border-radius: 40px;
-    border-radius: 40px;
-}
-.right {
-    float: right;
-}
-.scrolldown { padding-left:20px; color:#EDECE8; font-size:40px; font-weight:bold; vertical-align:middle;
-	text-shadow: #6374AB 2px 2px 2px;
- }
- .contentblock { margin: 0px 20px; }
- .results { border: 1px solid blue; padding:20px; margin-top:20px; min-height:50px; }
- .blue-bold { font-size:18px; font-weight:bold; color:blue; }
+	body {
+	    background-color: #C6F5F2;
+	}
+	#content { background-color:#C6F5F2; width:950px; min-height:550px; text-align:left; padding:20px;  }
+	h1 {
+	    padding:20px 10px 20px 10px;
+	}
+	h2 {
+	    padding-left: 0px !important;
+	}
+	#header {
+	    background-color: #CA410B !important;
+	}
+	.large { font-size:22px; }
+	.orange { color:orange; }
+	.italic { font-style:italic }
+	.textmiddle {vertical-align:middle;}
+	.padout { padding-left:25px; padding-right:25px; }
+	.rounded-corners {
+	     -moz-border-radius: 40px;
+	    -webkit-border-radius: 40px;
+	    -khtml-border-radius: 40px;
+	    border-radius: 40px;
+	}
+	.rounded-corners-top {
+	     -moz-border-top-radius: 40px;
+	    -webkit-border-radius: 40px;
+	    -khtml-border-radius: 40px;
+	    border-radius: 40px;
+	}
+	.right {
+	    float: right;
+	}
+	.scrolldown { padding-left:20px; color:#EDECE8; font-size:40px; font-weight:bold; vertical-align:middle;
+		text-shadow: #6374AB 2px 2px 2px;
+	 }
+	 .contentblock { margin: 0px 20px; }
+	 .results { border: 1px solid blue; padding:20px; margin-top:20px; min-height:50px; }
+	 .blue-bold { font-size:18px; font-weight:bold; color:blue; }
 
- /* example styles for validation form demo */
-#register-form {
-    background: url("form-fieldset.gif") repeat-x scroll left bottom #F8FDEF;
-    border: 1px solid #DFDCDC;
-    border-radius: 15px 15px 15px 15px;
-    display: inline-block;
-    margin-bottom: 30px;
-    margin-left: 20px;
-    margin-top: 10px;
-    padding: 25px 50px 10px;
-    width: 350px;
-}
+	 /* example styles for validation form demo */
+	#register-form {
+	    background: url("form-fieldset.gif") repeat-x scroll left bottom #F8FDEF;
+	    border: 1px solid #DFDCDC;
+	    border-radius: 15px 15px 15px 15px;
+	    display: inline-block;
+	    margin-bottom: 30px;
+	    margin-left: 20px;
+	    margin-top: 10px;
+	    padding: 25px 50px 10px;
+	    width: 350px;
+	}
 
-#register-form .fieldgroup {
-    background: url("form-divider.gif") repeat-x scroll left top transparent;
-    display: inline-block;
-    padding: 8px 10px;
-    width: 340px;
-}
+	#register-form .fieldgroup {
+	    background: url("form-divider.gif") repeat-x scroll left top transparent;
+	    display: inline-block;
+	    padding: 8px 10px;
+	    width: 340px;
+	}
 
-#register-form .fieldgroup label {
-    float: left;
-    padding: 15px 0 0;
-    text-align: right;
-    width: 110px;
-}
+	#register-form .fieldgroup label {
+	    float: left;
+	    padding: 15px 0 0;
+	    text-align: right;
+	    width: 110px;
+	}
 
-#register-form .fieldgroup input, #register-form .fieldgroup textarea, #register-form .fieldgroup select {
-    float: right;
-    margin: 10px 0;
-    height: 25px;
-}
+	#register-form .fieldgroup input, #register-form .fieldgroup textarea, #register-form .fieldgroup select {
+	    float: right;
+	    margin: 10px 0;
+	    height: 25px;
+	}
 
-#register-form .submit {
-    padding: 10px;
-    width: 220px;
-    height: 40px !important;
-}
+	#register-form .submit {
+	    padding: 10px;
+	    width: 220px;
+	    height: 40px !important;
+	}
 
-#register-form .fieldgroup label.error {
-    color: #FB3A3A;
-    display: inline-block;
-    margin: 4px 0 5px 125px;
-    padding: 0;
-    text-align: left;
-    width: 220px;
-}
-</style>
+	#register-form .fieldgroup label.error {
+	    color: #FB3A3A;
+	    display: inline-block;
+	    margin: 4px 0 5px 125px;
+	    padding: 0;
+	    text-align: left;
+	    width: 220px;
+	}
+	</style>
 </head>
 <body>
 	
 	<div id="page-wrap">
-
-
-		<div id="header">
-			<a href="http://www.jquery4u.com/"><img src="http://www.jquery4u.com/wp-content/uploads/2011/07/new-jquery-logo3.png" id="logo-image"></a>
-		</div>
 
 		<div id="content">
 
@@ -152,24 +147,11 @@ h2 {
 			</form>
 			<!-- END HTML form for validation -->
 
-
-			<p>
-				<a href="http://www.jquery4u.com/forms/basic-jquery-form-validation-tutorial/"><img src="http://www.jquery4u.com/demos/images/btn-tutorial-1.jpg" alt="tutorial" /></a>
-				<a href="http://www.jquery4u.com/demos/basic-jquery-validation-form/basic-jquery-validation-form-demo.zip"><img src="http://www.jquery4u.com/demos/images/btn-download-1.jpg" alt="download" /></a>
-			</p>
-
 		</div>
 
 	</div>
 
-</div> <!-- end page wrap -->
-
-<footer>
-	<p>&copy; Copyright 2012 jQuery4u. All rights reserved.</p>
-</footer>
-
-
-	<script type="text/javascript">
+<script type="text/javascript">
 /**
   * Basic jQuery Validation Form Demo Code
   * Copyright Sam Deering 2012
